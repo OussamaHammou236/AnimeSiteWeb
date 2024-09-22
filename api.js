16bb604155e52b64e28dfca53608ff703990a344
ohammou- <ohammou-@c4r6p3.1337.ma>
2024-09-22 18:43:46 +0100

    get info

--- api.js
+++ api.js
@@ -1,5 +1,14 @@
 const url = "https://api-anime-rouge.vercel.app/aniwatch/";
 
+
+async function get_info(parm)
+{
+    // const res = await fetch(`https://api-anime-rouge.vercel.app/aniwatch/anime/${parm}`);
+    // const data = await res.json();
+    console.log(parm);
+}
+
+
 async function get_data() {
     const res = await fetch(url);
     const data = await res.json();
@@ -8,12 +17,14 @@ async function get_data() {
     {
         document.getElementsByClassName("con1")[0].innerHTML += `
         <div class="anime">
-            <img src=${data.latestEpisodes[i].img} style="width: 90%;height: 80%;">
-            <h2></h2>
+            <img src=${data.latestEpisodes[i].img} style="width: 90%;height: 80%;  border-radius:20px;margin-top:5%;">
+            <p>${data.latestEpisodes[i].name}</p> 
         </div>`;
     }
-}
+    for(let i in data.latestEpisodes)
+        document.getElementsByClassName("anime")[i].addEventListener("click", () => get_info(data.latestEpisodes[i].id));
+
+}   
 
 get_data();
-
 
